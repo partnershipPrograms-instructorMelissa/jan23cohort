bc93e69ac54a31c6d74fe6b42bcc8756b0507731
mligon115 <mligon115@yahoo.com>
2023-03-19 13:21:45 -0500

    saving

--- assignments/java/CafeJava.java
+++ assignments/java/CafeJava.java
@@ -5,8 +5,8 @@ public class CafeJava {
         // APP VARIABLES
         // Lines of text that will appear in the app.
         String generalGreeting = "Welcome to Cafe Java, ";
-        String pendingMessage = ", your order will be ready shortly";
-        String readyMessage = ", your order is ready";
+        String pendingMessage = ", your order will be ready shortly.";
+        String readyMessage = ", your order is ready.";
         String displayTotalMessage = "Your total is $";
 
         // Menu variables (add yours below)
@@ -29,9 +29,28 @@ public class CafeJava {
 
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
-        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        // System.out.println(generalGreeting + customer1);
         // ** Your customer interaction print statements will go here ** //
-        System.out.println(customer1 + pendingMessage);
+        // System.out.println(customer1 + pendingMessage);
 
+        // ***** Noah Order *******//
+        // if (isReadyOrder4) {
+        // System.out.println(customer4 + readyMessage + displayTotalMessage +
+        // cappuccino);
+        // } else {
+        // System.out.println(customer4 + pendingMessage);
+        // }
+
+        // ***** Sam Order ****** //
+        // System.out.println(customer2 + ", " + displayTotalMessage + (latte * 2));
+        // if (isReadyOrder2) {
+        // System.out.println(customer2 + readyMessage + " " + displayTotalMessage +
+        // (latte * 2));
+        // } else {
+        // System.out.println(customer2 + pendingMessage);
+        // }
+
+        // ****** Jimmy Order ****** //
+        System.out.println(customer3 + " " + displayTotalMessage + (double) (latte - dripCofee));
     }
 }
