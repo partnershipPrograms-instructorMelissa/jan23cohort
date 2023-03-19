5352af53c9b64cd4095b18a1b1197ee639bb5db1
mligon115 <mligon115@yahoo.com>
2023-03-19 11:56:23 -0500

    saving

---
+++ assignments/java/Fundamentals/NameAge.java
@@ -0,0 +1,14 @@
+package assignments.java.Fundamentals;
+
+public class NameAge {
+    public static void main(String[] args) {
+
+        String name = "Mar'Kebta Goss";
+        int age = 38;
+        String hometown = "Detroit, MI";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+    }
+}
