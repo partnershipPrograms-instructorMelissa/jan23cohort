91d72a49ad9db3bc58086c8d32a5a7ae71bc8080
mligon115 <mligon115@yahoo.com>
2023-03-20 16:28:06 -0500

    saving

---
+++ assignments/java/Week1/ListException/TryCatch.java
@@ -0,0 +1,23 @@
+package assignments.java.Week1.ListException;
+
+import java.util.ArrayList;
+
+public class TryCatch {
+    public static void main(String[] args) {
+
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+            } catch (ClassCastException e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println("Finished");
+    }
+}
