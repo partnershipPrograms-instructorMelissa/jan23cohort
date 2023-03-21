60e69ad7ce4f59eb8bc13e883a8b14c69ba74843
mligon115 <mligon115@yahoo.com>
2023-03-20 21:18:14 -0500

    saving

--- assignments/java/Week1/BankApp/BankAccount.java
+++ assignments/java/Week1/BankApp/BankAccount.java
@@ -4,17 +4,41 @@ public class BankAccount {
 
     public double checkingBalance;
     public double savingsBalance;
+    public double deposit;
     public static int numberOfAccount = 0;
-    public static int totalAmountOfMoney = 0;
+    public static double totalAmountOfMoney = 0;
 
-    public BankAccount(double checkingBalance, double savingsBalance) {
-
-        this.checkingBalance = checkingBalance;
-        this.savingsBalance = savingsBalance;
+    public BankAccount() {
         numberOfAccount++;
+        totalAmountOfMoney += deposit;
     }
 
     public static void createdAccounts() {
         System.out.println("Total number of accounts: " + numberOfAccount);
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingBalance() {
+        return savingsBalance;
+    }
+
+    public static void setTotalAmountOfMoney(double inputAmount) {
+        totalAmountOfMoney += inputAmount;
+    }
+
+    public double depositFunds(double dollarAmount, String accountType) {
+
+        if (accountType.equals("checking")) {
+            this.checkingBalance += dollarAmount;
+            return checkingBalance;
+        } else if (accountType.equals("saving")) {
+            this.savingsBalance += dollarAmount;
+            return savingsBalance;
+        } else {
+            return -1;
+        }
     }
 }
---
+++ assignments/java/Week1/BankApp/Main.java
@@ -0,0 +1,15 @@
+package assignments.java.Week1.BankApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount account1 = new BankAccount();
+        System.out.println(account1.depositFunds(150, "checking"));
+        System.out.println(account1.depositFunds(250, "checking"));
+        System.out.println(account1.depositFunds(250, "saving"));
+        System.out.println(account1.depositFunds(250, "saving"));
+
+    }
+
+}
