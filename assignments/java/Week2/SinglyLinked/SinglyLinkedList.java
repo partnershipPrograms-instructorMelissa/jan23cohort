fc28cf109dd3f72708cc4c2861a62fae83693f6a
mligon115 <mligon115@yahoo.com>
2023-03-24 15:09:59 -0500

    saving

---
+++ assignments/java/Week2/SinglyLinked/SinglyLinkedList.java
@@ -0,0 +1,33 @@
+// package assignments.java.Week2.SinglyLinked;
+
+public class SinglyLinkedList {
+    public Node head;
+    public Node runner;
+
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+
+    // SLL methods go here. As a starter, we will show you how to add a node to the
+    // list.
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while (runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+
+    public void printValues() {
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+    }
+}
